af953d1348e5fa92e3f2daa998042f534068bd75
Abhinav Prakash <abhinav.prakash1@gmail.com>
2017-06-07 01:47:26 +0530

    Styled dropdown CSS so that dropdown and Submit are always in line.

--- chat/forms.py
+++ chat/forms.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class NetworkForm(forms.Form):
-    user_id = forms.ModelChoiceField(queryset=User.objects.all(), label='',
-                                       widget=forms.Select(attrs={'class': 'form-control'}))
+    user_id = forms.ModelChoiceField(queryset=User.objects.all(), empty_label='None', label='',
+                                     widget=forms.Select(attrs={'class': 'all_users'}))
 
