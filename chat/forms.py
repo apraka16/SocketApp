6fcad50457f46bda395b61c37f0384cc41f58ece
Abhinav Prakash <abhinav.prakash1@gmail.com>
2017-06-07 16:51:19 +0530

    Add in network feature added. User must be adding people first and then having a conversation. @TODO: Validation whether user actually want to be added.

--- chat/forms.py
+++ chat/forms.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 
 
 class NetworkForm(forms.Form):
+
+    def __init__(self, uid_list, *args, **kwargs):
+        super(NetworkForm, self).__init__(*args, **kwargs)
+        self.fields['user_id'].queryset = User.objects.filter(id__in=uid_list)
+
+    error_css_class = 'error'
+
     user_id = forms.ModelChoiceField(queryset=User.objects.all(), empty_label='None', label='',
-                                     widget=forms.Select(attrs={'class': 'all_users'}))
+                                     widget=forms.Select(attrs={'class': 'all_users'}),
+                                     error_messages={"invalid_choice": "User has already added you. Check out your network below."},
+                                     )
+
+
+
+
 
